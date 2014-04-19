2a72aac9a4b9d95d4034e7c6cad3a9bd0539c383
Michael Yagudaev <michael@yagudaev.com>
2014-04-18 18:21:11 -0700

    fixing gem

--- ext/antiword/extconf.rb
+++ ext/antiword/extconf.rb
@@ -2,8 +2,8 @@ require 'mkmf'
 
 root = File.expand_path('../../..', __FILE__)
 
-Dir.chdir(File.join(root, 'vendor/antiword')) do
+Dir.chdir(File.join(root, 'vendor/antiword-0.37')) do
   system "make"
 end
 
-create_makefile 'xpdf'
+create_makefile 'antiword'
