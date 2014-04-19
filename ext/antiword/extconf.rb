8343135d26dba8eb46667f3a48238223ea6cf051
Michael Yagudaev <michael@yagudaev.com>
2014-04-18 19:08:21 -0700

    also included the resources needed for antiword

--- ext/antiword/extconf.rb
+++ ext/antiword/extconf.rb
@@ -6,6 +6,7 @@ Dir.chdir(File.join(root, 'vendor/antiword-0.37')) do
   system "make"
   system "mkdir #{root}/bin"
   system "mv antiword #{root}/bin/"
+  system "mv -r Resources ~/.antiword"
 end
 
 create_makefile 'antiword'
