eff92e1d9988e168247d5292c1cb6cd663ad3f1a
Michael Yagudaev <michael@yagudaev.com>
2014-04-18 18:49:38 -0700

    fix destination directory

--- ext/antiword/extconf.rb
+++ ext/antiword/extconf.rb
@@ -1,9 +1,11 @@
 require 'mkmf'
 
-root = File.expand_path('../../..', __FILE__)
+root = File.expand_path('../..', __FILE__)
 
 Dir.chdir(File.join(root, 'vendor/antiword-0.37')) do
-  system "make HOME=#{root}"
+  system "make"
+  system "mkdir #{root}/bin"
+  system "mv antiword #{root}/bin/"
 end
 
 create_makefile 'antiword'
