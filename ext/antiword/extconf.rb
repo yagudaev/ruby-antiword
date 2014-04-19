04bf500fb050de6c52a2d5aa043c2aa6abccd3c7
Michael Yagudaev <michael@yagudaev.com>
2014-04-18 18:32:20 -0700

    get the executable to complie into the right directory

--- ext/antiword/extconf.rb
+++ ext/antiword/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 root = File.expand_path('../../..', __FILE__)
 
 Dir.chdir(File.join(root, 'vendor/antiword-0.37')) do
-  system "make"
+  system "make HOME=#{root}"
 end
 
 create_makefile 'antiword'
